5eda2c26902acb3006bacb400e1f26831b25065f
Barry <barry.brands@student.rocvf.nl>
2018-09-04 10:35:02 +0200

    Commit

---
+++ src/H04/Opdr45.java
@@ -0,0 +1,15 @@
+package H04;
+
+import java.awt.*;
+import java.applet.*;
+
+public class Opdr45 extends Applet {
+
+    public void paint(Graphics g) {
+
+        setBackground(Color.blue);
+        g.setColor(Color.yellow);
+        g.fillArc(100, 50, 100, 175, 0, 360);
+    }
+}
+
