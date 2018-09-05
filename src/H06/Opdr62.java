d0fe0f5e959815f2eec2a47619c224ad0aff71bd
Barry <barry.brands@student.rocvf.nl>
2018-09-05 09:45:39 +0200

    Commit

--- src/H06/Opdr62.java
+++ src/H06/Opdr62.java
@@ -1,4 +1,31 @@
 package H06;
 
-public class Opdr62 {
+import java.awt.*;
+import java.applet.*;
+
+public class Opdr62 extends Applet{
+
+    int secondesperuur;
+    int secondesperdag;
+    int secondesperjaar;
+
+
+    public void init() {
+
+        secondesperuur = 60*60;
+        secondesperdag = secondesperuur*24;
+        secondesperjaar = secondesperdag*364;
+    }
+
+    public void paint (Graphics g) {
+
+        g.drawString("Secondes per uur: " + secondesperuur,20,20);
+        g.drawString("Secondes per dag: " + secondesperdag,20,35);
+        g.drawString("Secondes per jaar: " + secondesperjaar, 20,50);
+    }
+
+
+
 }
+
+
