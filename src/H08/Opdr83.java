8d346ffda0404d64ff4589cc7b9ceee3af0e7466
SchoolAccB <bbrands@roc-dev.com>
2018-09-06 21:17:15 +0200

    initial.commit

---
+++ src/H08/Opdr83.java
@@ -0,0 +1,15 @@
+package H08;
+
+import java.awt.*;
+import java.applet.*;
+import java.awt.event.*;
+
+public class Opdr83 extends Applet {
+
+    public void init();
+
+    public void paint (Graphics g) {
+
+
+    }
+}
