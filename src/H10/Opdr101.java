e21f2e82f76a905b41262d1337b2b571c9b195a4
SchoolAccB <bbrands@roc-dev.com>
2018-09-10 11:05:03 +0200

    initial.commit

--- src/H10/Opdr101.java
+++ src/H10/Opdr101.java
@@ -6,12 +6,59 @@ import java.awt.event.*;
 
 public class Opdr101 extends Applet {
 
+    int input;
+    int hooggetal;
+
+    TextField tekstvak;
+    Button okKnop;
+    String s;
+
+
     public void init() {
+
+        tekstvak = new TextField("", 20);
+        tekstvak.addActionListener(new tekstvakListener());
+
+        add(tekstvak);
+
+        okKnop = new Button("Ok");
+        okKnop.addActionListener(new okKnopListener());
+        add(okKnop);
 
 
     }
-    public void paint (Graphics g) {
+
+    public void paint(Graphics g) {
+        g.drawString("De hoogste input is " + hooggetal, 100, 100);
 
 
+    }
+
+    class tekstvakListener implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
+            s = tekstvak.getText();
+            input = Integer.parseInt(s);
+            if (input > hooggetal) {
+                hooggetal = input;
+                s = "" + hooggetal;
+                repaint();
+            }
+
+        }
+    }
+
+
+    class okKnopListener implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
+            s = tekstvak.getText();
+            input = Integer.parseInt(s);
+            if (input > hooggetal) {
+                hooggetal = input;
+                s = "" + hooggetal;
+                repaint();
+            }
+
+
+        }
     }
 }
