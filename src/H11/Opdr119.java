f3cfe7413e538b50996f86eb41ed268eb9480cf4
SchoolAccB <bbrands@roc-dev.com>
2018-09-20 11:16:29 +0200

    initiate.commit

--- src/H11/Opdr119.java
+++ src/H11/Opdr119.java
@@ -7,14 +7,22 @@ public class Opdr119 extends Applet {
 
     int x;
     int x2;
+    int x3;
+    int x4;
     public void init() {
 
-        x = 20;
-        x2 = 40;
+
     }
     public void paint (Graphics g) {
 
+        x = 20;
+        x2 = 40;
+
+        x3 = 40;
+        x4 = 20;
+
         for (int teller = 0; teller < 4; teller++){
+
 
             x += 40;
             x2 += 40;
@@ -22,13 +30,90 @@ public class Opdr119 extends Applet {
             g.fillRect(x, 20, 21,21);
             g.drawRect(x2,20,20,20);
         }
-        for (int teller1 = 0; teller1 < 4; teller1++){
+        for (int teller = 0; teller < 4; teller++){
+
+            x3 += 40;
+            x4 += 40;
+            g.drawRect(x4,40,20,20);
+            g.setColor(Color.black);
+            g.fillRect(x3, 40, 21,21);
+
+        }
+
+        x = 20;
+        x2 = 40;
+
+        for (int teller = 0; teller < 4; teller++){
+
 
             x += 40;
             x2 += 40;
-            g.drawRect(x2,40,20,20);
             g.setColor(Color.black);
-            g.fillRect(x, 40, 21,21);
+            g.fillRect(x, 60, 21,21);
+            g.drawRect(x2,60,20,20);
+        }
+
+        x3 = 40;
+        x4 = 20;
+
+        for (int teller = 0; teller < 4; teller++){
+
+            x3 += 40;
+            x4 += 40;
+            g.drawRect(x4,80,20,20);
+            g.setColor(Color.black);
+            g.fillRect(x3, 80, 21,21);
+
+        }
+
+        x = 20;
+        x2 = 40;
+
+        for (int teller = 0; teller < 4; teller++){
+
+
+            x += 40;
+            x2 += 40;
+            g.setColor(Color.black);
+            g.fillRect(x, 100, 21,21);
+            g.drawRect(x2,100,20,20);
+        }
+
+        x3 = 40;
+        x4 = 20;
+
+        for (int teller = 0; teller < 4; teller++){
+
+            x3 += 40;
+            x4 += 40;
+            g.drawRect(x4,120,20,20);
+            g.setColor(Color.black);
+            g.fillRect(x3, 120, 21,21);
+
+        }
+
+        x = 20;
+        x2 = 40;
+
+        for (int teller = 0; teller < 4; teller++){
+
+
+            x += 40;
+            x2 += 40;
+            g.setColor(Color.black);
+            g.fillRect(x, 140, 21,21);
+            g.drawRect(x2,140,20,20);
+        }
+        x3 = 40;
+        x4 = 20;
+
+        for (int teller = 0; teller < 4; teller++){
+
+            x3 += 40;
+            x4 += 40;
+            g.drawRect(x4,160,20,20);
+            g.setColor(Color.black);
+            g.fillRect(x3, 160, 21,21);
 
         }
 
