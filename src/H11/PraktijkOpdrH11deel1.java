36e9200df3ece9f115054bd74cd7982897bdea05
SchoolAccB <bbrands@roc-dev.com>
2018-09-21 12:33:06 +0200

    initiate.commit

---
+++ src/H11/PraktijkOpdrH11deel1.java
@@ -0,0 +1,63 @@
+package H11;
+
+import java.awt.*;
+import java.applet.*;
+import java.awt.event.*;
+
+public class PraktijkOpdrH11deel1 extends Applet {
+
+    Button okKnop;
+    TextField tekstvak;
+
+    int input;
+    int uitkomst;
+    int teller;
+
+    int y;
+
+    public void init() {
+        setSize(600, 400);
+
+        tekstvak = new TextField("", 15);
+        tekstvak.addActionListener(new tekstvakListener());
+        add(tekstvak);
+
+        okKnop = new Button("Ok");
+        okKnop.addActionListener(new okKnopListener());
+        add(okKnop);
+
+
+    }
+
+    public void paint(Graphics g) {
+
+        y = 50;
+
+        for (int teller = 1; teller < 11; teller++) {
+            y += 15;
+            uitkomst = teller * input;
+            g.drawString("" + teller + " * " + input + " = " + uitkomst, 20, y);
+        }
+    }
+
+    class tekstvakListener implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
+            input = Integer.parseInt(tekstvak.getText());
+
+            tekstvak.setText("");
+            repaint();
+        }
+    }
+
+
+    class okKnopListener implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
+            input = Integer.parseInt(tekstvak.getText());
+
+            tekstvak.setText("");
+            repaint();
+        }
+    }
+}
+
+
