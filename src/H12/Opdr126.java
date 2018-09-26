c67a5099d273d7ed9d1ba1eab998f4d038b78bd6
SchoolAccB <bbrands@roc-dev.com>
2018-09-26 11:21:39 +0200

    initiate.commit

--- src/H12/Opdr126.java
+++ src/H12/Opdr126.java
@@ -9,13 +9,76 @@ public class Opdr126 extends Applet {
 
     boolean gevonden;
     int gezocht;
+    int keerGevonden;
 
     int[] tabel = {300, 300, 300, 600, 600, 600, 900, 900, 900, 1000, 1000};
 
+    TextField tekstvak;
+    Button zoekKnop;
+
     public void init () {
 
+        tekstvak = new TextField(20);
+        tekstvak.addActionListener(new tekstvakListener());
+        add(tekstvak);
+
+        zoekKnop = new Button("Zoek");
+        zoekKnop.addActionListener(new zoekKnopListener());
+        add(zoekKnop);
     }
     public void paint (Graphics g) {
 
+        for (int teller = 0; teller < tabel.length; teller++) {
+            g.drawString("" + tabel[teller], 50, 20 * teller + 60);
+        }
+        if(gevonden == true) {
+            g.drawString("De waarde " + gezocht + " is " + keerGevonden + "x gevonden.", 100, 60);
+        }
+        else {
+            g.drawString("De waarde is niet gevonden.", 100, 60);
+        }
+
+    }
+    class tekstvakListener implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
+            gezocht = Integer.parseInt(tekstvak.getText());
+
+            gevonden = false;
+
+            int teller = 0;
+            while(teller < tabel.length) {
+                if(tabel[teller] == gezocht) {
+                    gevonden = true;
+                    keerGevonden = keerGevonden + 1;
+                }
+                teller ++;
+                }
+
+
+            tekstvak.setText("");
+            repaint();
+
+        }
+    }
+    class zoekKnopListener implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
+            gezocht = Integer.parseInt(tekstvak.getText());
+
+            gevonden = false;
+
+            int teller = 0;
+            while(teller < tabel.length) {
+                if(tabel[teller] == gezocht) {
+                    gevonden = true;
+                    keerGevonden = keerGevonden + 1;
+                }
+                teller ++;
+            }
+
+
+            tekstvak.setText("");
+            repaint();
+
+        }
     }
 }
