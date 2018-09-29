862420b4499fca79c585a736e3736cb102ef58c7
SchoolAccB <bbrands@roc-dev.com>
2018-09-29 15:52:31 +0200

    initiate.commit

---
+++ src/H13/Opdr132.java
@@ -0,0 +1,19 @@
+package H13;
+
+import java.awt.*;
+import java.applet.*;
+
+public class Opdr132 extends Applet {
+    int x, y;
+
+    public void init() {
+
+    }
+    public void paint(Graphics g) {
+        tekenMuur();
+
+    }
+    public void tekenMuur() {
+
+    }
+}
