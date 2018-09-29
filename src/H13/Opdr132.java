c3064bf1ed4372d29e182a32ff0093136157cf60
SchoolAccB <bbrands@roc-dev.com>
2018-09-29 21:30:17 +0200

    initiate.commit

--- src/H13/Opdr132.java
+++ src/H13/Opdr132.java
@@ -4,16 +4,32 @@ import java.awt.*;
 import java.applet.*;
 
 public class Opdr132 extends Applet {
-    int x, y;
+    int x, y, width, height;
 
     public void init() {
+        width = 100;
+        height = 33;
+
 
     }
     public void paint(Graphics g) {
-        tekenMuur();
-
+        for (int teller = 0; teller < 10; teller++) {
+            x = 0;
+            for (teller = 0; teller < 5; teller++) {
+                tekenMuur();
+                x += 100;
+            }
+            y += 33;
+            x = -50;
+            for (teller = 0; teller < 5; teller++) {
+                tekenMuur();
+                x += 100;
+            }
+            y += 33;
+        }
     }
     public void tekenMuur() {
+        getGraphics().drawRect(x, y, width, height);
 
     }
 }
